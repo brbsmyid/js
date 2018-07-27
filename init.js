07ce9fff6a563649e9f5a9a7b6a03bdea067008e
brbsmyid <37326385+brbsmyid@users.noreply.github.com>
2018-07-27 15:37:49 +0700

    Add files via upload

---
+++ init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+	$('.modal').modal();
+    $('.carousel.carousel-slider').carousel();
+	 $('.slider').slider();
+	$('.materialboxed').materialbox();
+	$('.collapsible').collapsible();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
