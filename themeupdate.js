8471a310733f5548837c62826668a96be474f084
brbsmyid <37326385+brbsmyid@users.noreply.github.com>
2026-02-20 05:23:19 +0700

    Update themeupdate.js

--- themeupdate.js
+++ themeupdate.js
@@ -13,7 +13,7 @@
     // ──────────────────────────────────────
     const _cfg = {
         // Encode: btoa(JSON.stringify(['domain1.com','domain2.com']))
-        domains: "WyJoZC1kcml2ZS5ibG9nc3BvdC5jb20iXQ==",
+        domains: "c2RhZGFzZi5jb20=",
         validKey: "UFJPUC1MT1JELTIwMjYtVjMtMUQ1NjZCOEQ=",
         storageKey: "cGxfbGljZW5zZV92ZXJpZmllZA==",
         version: "MS4wLjE=",
